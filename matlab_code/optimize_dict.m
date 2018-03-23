ba4bc2c68228a2a77d63197d6bb05f437ec8c257
Rhiannon Tully-Barr <rtullybarr@gmail.com>
2018-03-23 10:10:45 -0700

    Fixing order of parameters

--- matlab_code/optimize_dict.m
+++ matlab_code/optimize_dict.m
@@ -1,4 +1,4 @@
 function dictionary = optimize_dict(dictionary, dictionary_assignments, feature_descriptors)
 % Optimize the dictionary using lagrange dual
-    dictionary = lagrange_dual(dictionary', dictionary_assignments, feature_descriptors, 1);
+    dictionary = lagrange_dual(feature_descriptors, dictionary_assignments, 1, dictionary);
 end
