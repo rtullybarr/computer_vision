02ed728ff5f0e13f3cfa83da7c9c508217869305
Madeleine <mtownley@hotmail.ca>
2018-03-23 19:22:58 -0700

    LBP outputs 3D matrix
    LBP outputs RxCx59 matrix with features organized correctly. To change back to a 2D matrix:
    features = reshape(features, [R*C, 59])

--- matlab_code/LBP.m
+++ matlab_code/LBP.m
@@ -5,6 +5,8 @@
 %   1. cellsize: 2x2 array, size of cells, default = [16,16]
 %   2. filt: parameter for filtering, default = 0 (no filtering)
 %   3. rot: whether to implement rotation invariance default = 0 (false)
+% OUTPUT: RxCx59 matrix of LBP features (or RxCx10 for rot. invariant)
+%         Change to 2D matrix: features = reshape(features, [RxC, 59]) 
 
 
 function features = LBP(img, varargin)
@@ -15,9 +17,7 @@ function features = LBP(img, varargin)
     cellsize = Defaults{1};
     filt = Defaults{2};
     rot = Defaults{3};
-    
-    %img = imread(filename);
-    %img = rgb2gray(img);
+
     s = size(img);
     r = floor(s(1)/cellsize(1));
     c = floor(s(2)/cellsize(2));
@@ -33,16 +33,16 @@ function features = LBP(img, varargin)
     % No rotational invariance, 59 bins for each cell
     if(rot == 0) 
         features = extractLBPFeatures(img,'CellSize', cellsize);
-        %features = reshape(features, [r, c, 59]);
         features = reshape(features, [59, length(features)/59]);
         features = features';
-        
+        features = reshape(features, [r, c, 59]);
    
     % Make LBP features rotationally invariant, 10 bins for each cell
     else 
         features = extractLBPFeatures(img,'CellSize', cellsize, 'Upright', false);
-        %features = reshape(features, [r, c, 10]);
         features = reshape(features, [legnth(features)/10, 10]);
+        features = features';
+        features = reshape(features, [r, c, 10]);
     end
     
     % 3. tri-linear interpolation?
