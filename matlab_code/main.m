a130f78a065ebf6933f4c06779136a28916a1819
Rhiannon Tully-Barr <rtullybarr@gmail.com>
2018-04-01 12:29:33 -0700

    Adding loop to main to more easily run all species at once.

--- matlab_code/main.m
+++ matlab_code/main.m
@@ -17,21 +17,21 @@
     
     all_images = [wildebeest; guineaFowl; hartebeest; giraffe];
     
-    for positive_class = 3:4
+    for positive_class = 1
         class_labels = zeros(length(all_images), 1);
         class_labels(species_masks == positive_class) = 1;
 
         fprintf("Extracting local binary patterns.\n");
         tic
-        LBP_features = cell(length(all_images), 1);
-        for i = 1:length(all_images)
-            LBP_features{i} = feats.LBP(all_images{i});
-        end
+%         LBP_features = cell(length(all_images), 1);
+%         for i = 1:length(all_images)
+%             LBP_features{i} = feats.LBP(all_images{i});
+%         end
         toc
 
         fprintf("Learning dictionary - LBP.\n");
         tic
-        LBP_dictionary = dict.learn_dictionary(LBP_features, training_set_size, dictionary_size, dictionary_iterations, lambda);
+%         LBP_dictionary = dict.learn_dictionary(LBP_features, training_set_size, dictionary_size, dictionary_iterations, lambda);
         toc
 
         fprintf("Extracting SIFT descriptors.\n");
@@ -54,14 +54,14 @@
         % step is dict_size + 4*dict_size + 16*dict_size
         img_vector_len = 21*dictionary_size;
 
-        LBP_image_vectors = zeros(length(LBP_features), img_vector_len);
-        for i = 1:length(LBP_features)
+%         LBP_image_vectors = zeros(length(LBP_features), img_vector_len);
+%         for i = 1:length(LBP_features)
             % Use SPM to get a single image vector
-            LBP_image_vectors(i, :) = dict.spatial_pyramid_matching(LBP_dictionary, LBP_features{i}, lambda);
-        end
+%             LBP_image_vectors(i, :) = dict.spatial_pyramid_matching(LBP_dictionary, LBP_features{i}, lambda);
+%         end
 
-        SIFT_image_vectors = zeros(length(LBP_features), img_vector_len);
-        for i = 1:length(LBP_features)
+        SIFT_image_vectors = zeros(length(SIFT_features), img_vector_len);
+        for i = 1:length(SIFT_features)
             % Use SPM to get a single image vector
             SIFT_image_vectors(i, :) = dict.spatial_pyramid_matching(SIFT_dictionary, SIFT_features{i}, lambda);
         end
@@ -78,8 +78,8 @@
         split = floor(length(class_labels) * 0.7);
 
         % training set
-        LBP_X_train = LBP_image_vectors(perm(1:split), :);
-        LBP_X_test = LBP_image_vectors(perm(split + 1:end), :);
+%         LBP_X_train = LBP_image_vectors(perm(1:split), :);
+%         LBP_X_test = LBP_image_vectors(perm(split + 1:end), :);
 
         SIFT_X_train = SIFT_image_vectors(perm(1:split), :);
         SIFT_X_test = SIFT_image_vectors(perm(split + 1:end), :);
@@ -88,8 +88,8 @@
         Y_test = class_labels(perm(split + 1:end));
 
         % LBP
-        LBP_model = svm.train(LBP_X_train, Y_train);
-        [LBP_precision, LBP_recall] = svm.evaluate_model(LBP_model, LBP_X_test, Y_test);
+%         LBP_model = svm.train(LBP_X_train, Y_train);
+%         [LBP_precision, LBP_recall] = svm.evaluate_model(LBP_model, LBP_X_test, Y_test);
 
         % SIFT
         SIFT_model = svm.train(SIFT_X_train, Y_train);
@@ -107,10 +107,10 @@
 
         suffix = [char(species_names(positive_class)) '_dictsize_' num2str(dictionary_size) '_iter_' num2str(dictionary_iterations) '_lambda_' num2str(lambda)];
     
-        save(['LPB_dict_' suffix], 'LBP_dictionary');
-        save(['LPB_img_vec_' suffix], 'LBP_image_vectors');
-        save(['SIFT_dict_' suffix], 'LBP_dictionary');
-        save(['SIFT_img_vec_' suffix], 'LBP_image_vectors');
+%         save(['LPB_dict_' suffix], 'LBP_dictionary');
+%         save(['LPB_img_vec_' suffix], 'LBP_image_vectors');
+        save(['SIFT_dict_' suffix], 'SIFT_dictionary');
+        save(['SIFT_img_vec_' suffix], 'SIFT_image_vectors');
         toc
      end
 %end
