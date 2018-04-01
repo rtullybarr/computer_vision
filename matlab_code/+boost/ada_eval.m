09a4289775d3bb91b3ece4fe04972d8d91ecf209
Madeleine <mtownley@hotmail.ca>
2018-04-01 15:15:32 -0700

    change ada_prep and ada_eval
    ada_eval takes premuted/separated data
    ada_prep takes one set of data, formats into cell array

--- matlab_code/+boost/ada_eval.m
+++ matlab_code/+boost/ada_eval.m
@@ -1,7 +1,8 @@
-function [precision, recall] = ada_eval(LBP_image_vectors, SIFT_image_vectors, class_labels)
+function [precision, recall] = ada_eval(LBP_train, LBP_test, SIFT_train, SIFT_test, test_labels, train_labels) 
 tic
     % prep data for adaboost
-    [training_set, testing_set, Y_test] = ada_prep(LBP_image_vectors, SIFT_image_vectors, class_labels);
+    training_set = ada_prep(LBP_train, SIFT_train, train_labels);
+    testing_set = ada_prep(LBP_test, SIFT_test, test_labels);
     
     % Create adaboosted classifer
     [ada_labels, h_model, h_weights, alpha]= boost.ada_train(training_set);
--- matlab_code/+boost/ada_predict.m
+++ matlab_code/+boost/ada_predict.m
@@ -7,10 +7,10 @@
 % testing set -> cell array of (M training samples) x (N feature types)
 
 function labels = ada_predict(model, alpha, h_weights, testing_set)
-    M = size(testing_set, 1); % number of training samples
-    N = size(testing_set, 2); % number of different feature types
-    T = size(weights, 2);     % number of intermediate classifiers, or trials used in ada_train  
-    H_test = zeros(M, T);     % set of predictions from each intermediate classifier  
+    M = size(testing_set, 1);   % number of training samples
+    N = size(testing_set, 2)-1; % number of different feature types, excluding class labels
+    T = size(weights, 2);       % number of intermediate classifiers, or trials used in ada_train  
+    H_test = zeros(M, T);       % set of predictions from each intermediate classifier  
     
     for t = 1:T
         H_test(:,t) = combo_predict(model{t}, training_set, N, h_weights(:,t));
