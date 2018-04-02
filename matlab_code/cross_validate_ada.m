e15a0122f0f43e7277ac56e2aa8335d9d48c82f4
AlexKoszegi <alex.koszegi90@gmail.com>
2018-04-01 20:23:18 -0700

    Tested cross_validate_ada

--- matlab_code/cross_validate_ada.m
+++ matlab_code/cross_validate_ada.m
@@ -11,14 +11,14 @@
 % OUTPUT
 % trained_model - cell array with {ada_labels, h_model, h_weights,alpha}
 
-function [trained_model] = cross_validate_ada(k, class_labels,...
+function trained_model = cross_validate_ada(k, class_labels,...
     LBP_image_vectors, SIFT_image_vectors)
 
     partition = 1/k;
-    ada_labels = zeros(k:1);
-    h_model = zeros(k:1);
-    h_weights = zeros(k:1);
-    alpha = zeros(k:1);
+    ada_labels =  cell(1,k);
+    h_model = cell(1,k);
+    h_weights = cell(1,k);
+    alpha = cell(1,k);
     
     % Get random order for cross validation
     perm = randperm(length(class_labels));
@@ -29,19 +29,24 @@ function [trained_model] = cross_validate_ada(k, class_labels,...
     for i = 1:k
         % Reorder image vectors and take a subset
         LBP_samples = LBP_image_vectors(perm(1:end), :);
-        LBP_samples((i-1)*split_interval+1:i*split_interval) = [];
+        LBP_samples((i-1)*split_interval+1:i*split_interval, :) = [];
         
         SIFT_samples = SIFT_image_vectors(perm(1:end), :);
-        SIFT_samples((i-1)*split_interval+1:i*split_interval) = [];
+        SIFT_samples((i-1)*split_interval+1:i*split_interval, :) = [];
+        
+        labels = class_labels(perm(1:end), :);
+        labels((i-1)*split_interval+1:i*split_interval, :) = [];
         
         % Format data for adaboost training
-        ada_data = ada_prep(LBP_samples, SIFT_samples, class_labels);
-        
-        % Train data
-        [ada_labels(i), h_model(i), h_weights(i), alpha(i)]= ada_train(ada_data);
+        ada_data = boost.ada_prep(LBP_samples, SIFT_samples, labels);
+
+
+    % Train data
+    [ada_labels{1,i}, h_model{1,i}, h_weights{1,i}, alpha{1,i}]= boost.ada_train(ada_data);
         
     end
     
     trained_model = {ada_labels, h_model, h_weights,alpha};
+
 
 end
