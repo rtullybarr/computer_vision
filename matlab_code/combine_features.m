b06af89388515326aac302d82654f5806b783bab
Madeleine <mtownley@hotmail.ca>
2018-04-03 19:15:58 -0700

    deleting extraneous files

--- matlab_code/combine_features.m
+++
@@ -1,6 +0,0 @@
-function boosted_features = combine_features(predictorData, classLabels)
-%COMBINE_FEATURES combines the LBP and HOG features using the Gentle Boost algorithm
-
-boosted_features = fitcensemble(predictorData,classLabels,'Method','GentleBoost', ...
-    'NumLearningCycles',200);
-end
